265069d304c4f9efcc88924956c8deb49e3342b8
Lehm12 <afda8754@chiba-u.jp>
2021-06-19 16:41:48 +0900

    step22

--- DeZero.py
+++ DeZero.py
@@ -3,6 +3,7 @@ import weakref
 import contextlib
 
 class Variable:
+    __array_priority__ = 200
     def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
@@ -86,9 +87,18 @@ class Variable:
         p = str(self.data).replace('\n', '\n' + ' ' * 9)
         return 'variable(' + p + ')'
 
+    def __mul__(self, other):
+        return mul(self, other)
+
+def as_Variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
 
 class Function:
     def __call__(self, *inputs): #p.11
+        inputs = [as_Variable(x) for x in inputs]
+
         xs = [x.data for x in inputs]
         ys = self.forward(*xs)
         if not isinstance(ys, tuple):
@@ -135,6 +145,7 @@ class Add (Function):
         return gy, gy
 
 def add(x0, x1):
+    x1 = as_array(x1)
     return Add()(x0, x1)
 
 class Square(Function):
@@ -170,3 +181,89 @@ def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0,x1)
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+def neg(x):
+    return Neg()(x)
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0,x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy /x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c-1) * gy
+        return gx
+
+def pow(x,c):
+    return Pow(c)(x)
+
+Variable.__mul__ = mul
+Variable.__add__ = add
+Variable.__rmul__ = mul
+Variable.__radd__ = add
+Variable.__neg__ = neg
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
